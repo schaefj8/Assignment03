649fcd3ed03ef3868a5a25602905fede515e7ef6
schaefj8 <35738257+schaefj8@users.noreply.github.com>
2018-01-28 13:05:52 -0500

    update tow of Main.java

--- Assignment03/src/primeOfBigInt/Main.java
+++ Assignment03/src/primeOfBigInt/Main.java
@@ -12,7 +12,7 @@ public class Main {
 		System.out.println("Testing to show a composite fails:");
 		System.out.println("Expected output: Failed");
 		BigInteger A = BigInteger.valueOf(12);
-		if(new // input().isBigPrime(A)) {
+		if(new // input().// method(A)) {
 			System.out.println("Test: Passed - is a prime");
 			
 		} else {
@@ -24,7 +24,7 @@ public class Main {
 		System.out.println("Testing to show a prime passes:");
 		System.out.println("Expected output: Passed");
 		BigInteger B = BigInteger.valueOf(13);
-		if(new // input().isBigPrime(B)) {
+		if(new // input().// method(B)) {
 			System.out.println("Test: Passed - is a prime");
 			
 		} else {
@@ -36,7 +36,7 @@ public class Main {
 		System.out.println("Testing to show a BigInt prime is efficient and passes:");
 		System.out.println("Expected output: Passed");
 		BigInteger C = BigInteger.valueOf(122949829);
-		if(new // input().isBigPrime(B)) {
+		if(new // input().// method(B)) {
 			System.out.println("Test: Passed - is a prime");
 			
 		} else {
@@ -48,7 +48,7 @@ public class Main {
 		System.out.println("Testing to show a BigInt prime passes as an extreme:");
 		System.out.println("Expected output: Passed");
 		BigInteger D = BigInteger.valueOf(879190841);
-		if(new // input().isBigPrime(D)) {
+		if(new // input().// method(D)) {
 			System.out.println("Test: Passed - is a prime");
 			
 		} else {
